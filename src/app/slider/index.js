9cef3f321faebf45c75d57417cc72600ee886a5b
Oksana Shtelma <oshtelma@gmail.com>
2024-06-15 17:13:36 +0300

    swiper

---
+++ src/app/slider/index.js
@@ -0,0 +1,24 @@
+import "./slider.css";
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination],
+
+  direction: "horizontal",
+  loop: true,
+  autoHeight: true,
+  
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+    dynamicMainBullets: true,
+    
+  },
+});
