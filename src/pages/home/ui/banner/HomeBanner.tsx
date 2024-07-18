ccfc7b5bacbce5c2347eb930c62e9ef250e71ef7
Valerii Strilets <valerii.strilets@gmail.com>
2024-07-18 15:13:59 +0300

    refactor

--- src/pages/home/ui/banner/HomeBanner.tsx
+++ src/pages/home/ui/banner/HomeBanner.tsx
@@ -3,13 +3,18 @@ import styles from "./styles.module.scss";
 export const HomeBanner = () => {
   return (
     <div className={styles.banner}>
-      <div className={styles.textBanner}> 
-      <div>Invite Friends and get 50% off on your next purchase</div>
-      <a className={styles.linkBanner} href="#">Invite Now</a>
+      <div className={styles.textBanner}>
+        <div>Invite Friends and get 50% off on your next purchase</div>
+        <a className={styles.linkBanner} href="#">
+          Invite Now
+        </a>
       </div>
-     
       <button type="button" className={styles.buttonBanner}>
-        <img className={styles.closeImg} src="/images/icon_close.svg" alt="close window" />
+        <img
+          className={styles.closeImg}
+          src="/images/icon_close.svg"
+          alt="close window"
+        />
       </button>
     </div>
   );
--- src/pages/home/ui/slider/HomeSlider.tsx
+++ src/pages/home/ui/slider/HomeSlider.tsx
@@ -1,14 +1,12 @@
 import Swiper from "swiper";
 import { clsx } from "clsx";
+import { useEffect, useRef } from "react";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import styles from "./styles.module.scss";
-import { Children, useEffect, useRef } from "react";
-
-import { Button } from "../../../../shared/components/button/Button";
-
+import { Button } from "@/shared/components/button/Button";
 
 export const HomeSlider = () => {
   const swiperRef = useRef<HTMLDivElement>(null);
@@ -34,69 +32,65 @@ export const HomeSlider = () => {
   return (
     <div ref={swiperRef} className={clsx("swiper", styles.slider)}>
       <div className={clsx("swiper-wrapper", styles.wrapper)}>
-
-
         <div className={clsx("swiper-slide", styles.slide)}>
-          <div className={clsx(styles.container)} >
-            <div className= {clsx(styles.girlPrada)}>
+          <div className={styles.container}>
+            <div className={styles.girlPrada}>
               <img
-                className={clsx(styles.imgGirl)}     
+                className={styles.imgGirl}
                 src="/slides/girl_prada.jpg"
                 alt="Prada girl"
               />
             </div>
-            <div className= {clsx(styles.backgroundPrada)}>
-              <div className={clsx(styles.textPrada)} >
+            <div className={styles.backgroundPrada}>
+              <div className={styles.textPrada}>
                 <img
-                  className= {clsx(styles.imgTextPrada)}  
+                  className={styles.imgTextPrada}
                   src="/slides/vector_prada.svg"
                   alt="title Prada"
                 />
               </div>
               <div>Big Fashion Festival</div>
               <div>50% - 80% off</div>
-              <Button className={clsx("button", styles.blackButton)}>Explore</Button>
-              
+              <Button color="dark">Explore</Button>
             </div>
           </div>
         </div>
 
-
         <div className={clsx("swiper-slide", styles.slide)}>
-          <div className={clsx(styles.backgroundGirl)}>
-            <div className={clsx(styles.backgroundDark)}>
-              <div className={clsx(styles.flexDark)}>
-                <div className={clsx(styles.foreverTitle)}>FOREVER 21</div>
+          <div className={styles.backgroundGirl}>
+            <div className={styles.backgroundDark}>
+              <div className={styles.flexDark}>
+                <div className={styles.foreverTitle}>FOREVER 21</div>
                 <div>Big Fashion Festival</div>
                 <div>70% - 80% off</div>
-                <Button className={clsx("button", styles.whiteButton)}>Explore</Button>
+                <Button className={clsx("button", styles.whiteButton)}>
+                  Explore
+                </Button>
               </div>
             </div>
           </div>
         </div>
 
-
         <div className={clsx("swiper-slide", styles.slide)}>
-          <div className={clsx(styles.containerForever21)}>
-            <div className={clsx(styles.girlForever21)}>
+          <div className={styles.containerForever21}>
+            <div className={styles.girlForever21}>
               <img
-                className={clsx(styles.girlForever21Img)}
+                className={styles.girlForever21Img}
                 src="/slides/girl_forever21.jpg"
                 alt="girl in green dress"
               />
             </div>
-            <div className={clsx(styles.textForever21)}>
-              <div className={clsx(styles.foreverTitle)}>FOREVER 21</div>
+            <div className={styles.textForever21}>
+              <div className={styles.foreverTitle}>FOREVER 21</div>
               <div>Big Fashion Festival</div>
               <div>50% - 80% off</div>
-             <Button className={clsx("button", styles.whiteButton)}>Explore Now</Button>
+              <Button className={styles.exploreButton}>Explore Now</Button>
             </div>
           </div>
         </div>
-
       </div>
 
-      <div className={clsx("swiper-pagination","bullet-active")}></div>
+      <div className={clsx("swiper-pagination", "bullet-active")}></div>
     </div>
   );
 };
